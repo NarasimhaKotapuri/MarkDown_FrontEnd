9ba6175d847ceae61513651216c5a3474d50b64c
NarasimhuluDvara <narasimhulu.kotapuri@dvara.com>
2024-10-02 19:38:34 +0530

    added option for process the .md file at backend

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import { useState } from 'react';
+
 import './App.css';
 
 function App() {
   const [markDown, setMarkDown] = useState("Welcome to MarkDown")
+  const [markDownBackend, setMarkDownBackend] = useState("Welcome to MarkDown")
+  const [file, setFile] = useState(null);
 
   const parseMarkdown = (text) => {
     const lines = text.split('\n');
@@ -75,11 +78,62 @@ function App() {
 
     return htmlLines.join('');
   };
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+
+    // Check if the uploaded file is a Markdown file
+    if (file && file.type === 'text/markdown' || file.name.endsWith('.md')) {
+      setFile(file);
+    } else {
+      alert('Please upload a valid Markdown (.md) file.');
+    }
+  };
+  const handleUpload = async () => {
+    if (!file) {
+      alert('Please select a file first.');
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('file', file); // Append the file to the FormData object
+
+    try {
+      const response = await fetch('http://localhost:4000/processFile', {
+        method: 'POST',
+        body: formData,
+      });
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      const data = await response.json();
+      if(data.message){
+        setMarkDownBackend(data.html)
+      }
+    } catch (error) {
+      console.error('Error uploading file:', error);
+      alert('Error uploading file. Please try again.');
+    }
+  };
   return (
     <>
+      <div className='container'>
+        <input 
+          className='file-upload'
+          placeholder='Upload .md file and submit to process at backend'
+          type='file'
+          accept=".md"
+          onChange={handleFileChange}
+        />
+        <button className='button' onClick={e=>{handleUpload()}}>Process at Backend</button>
+      </div>
       <div className='center-div'>
-        <textarea className='left-side' value={markDown} onChange={e=>setMarkDown(e.target.value)} />
-        <div dangerouslySetInnerHTML={{ __html: parseMarkdown(markDown) }}  className='right-side' />
+        <textarea className='left-side' value={markDown} onChange={e=>{setMarkDown(e.target.value);setFile(null);setMarkDownBackend('')}} />
+        {file && markDownBackend
+          ?<div className='right-side' dangerouslySetInnerHTML={{ __html: parseMarkdown(markDownBackend)}} />
+          :<div dangerouslySetInnerHTML={{ __html: parseMarkdown(markDown)}} className='right-side' />
+        }
       </div>
     </>
   );
